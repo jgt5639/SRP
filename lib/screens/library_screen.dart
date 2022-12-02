53827f60b5d14b40e460ca72ddd4b2af871c20fc
unknown <giancarlo.perez01@utrgv.edu>
2022-12-02 10:30:51 -0600

    scrolly name

--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:text_scroll/text_scroll.dart';
 
 class LibraryScreen extends StatefulWidget {
   const LibraryScreen({Key? key}) : super(key: key);
@@ -121,9 +122,8 @@ class LibraryScreenState extends State<LibraryScreen> {
         backgroundColor: bgColor,
         body: SingleChildScrollView(
           child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            padding: const EdgeInsets.only(top: 20.0, right: 20.0, left: 20.0),
+            width: double.infinity,
+            padding: const EdgeInsets.only(top: 50.0, right: 20.0, left: 20.0),
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.topCenter,
@@ -183,8 +183,13 @@ class LibraryScreenState extends State<LibraryScreen> {
                                   margin: const EdgeInsets.only(
                                       top: 15, left: 33, right: 2),
                                   child: Column(children: [
-                                    Text(
-                                      overflow: TextOverflow.ellipsis,
+                                    TextScroll(
+                                      delayBefore: Duration(milliseconds: 2000),
+                                      pauseBetween:
+                                          Duration(milliseconds: 3000),
+                                      intervalSpaces: 10,
+                                      velocity: Velocity(
+                                          pixelsPerSecond: Offset(50, 0)),
                                       textAlign: TextAlign.left,
                                       currentSongTitle,
                                       style: const TextStyle(
@@ -526,6 +531,7 @@ class LibraryScreenState extends State<LibraryScreen> {
                           selectedTileColor: fiveColor,
                           textColor: fourColor,
                           title: Text(
+                            overflow: TextOverflow.ellipsis,
                             item.data![index].title,
                             style: const TextStyle(
                                 fontFamily: 'Montserrat',
@@ -533,6 +539,7 @@ class LibraryScreenState extends State<LibraryScreen> {
                                 fontWeight: FontWeight.w600),
                           ),
                           subtitle: Text(
+                            overflow: TextOverflow.ellipsis,
                             item.data![index].displayNameWOExt,
                             style: const TextStyle(
                               fontFamily: 'Montserrat',
